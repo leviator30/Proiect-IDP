aab449ce2f5290cd2be13491ae813e33483ddff7
leviator30 <leviator_30@yahoo.com>
2026-04-06 00:39:55 +0300

    ci/cd_1.2

--- auth-service/tests/test_main.py
+++ auth-service/tests/test_main.py
@@ -1,3 +1,44 @@
-def test_smoke():
-    # Un test simplu care trece mereu, doar ca să verificăm pipeline-ul
-    assert 1 == 1
+import pytest
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_register_user():
+    payload = {"username": "test1", "password": "password1"}
+    response = client.post("/register", json=payload)
+    
+    assert response.status_code == 201
+    assert response.json()["username"] == "testuser"
+
+def test_register_duplicate_user():
+    payload = {"username": "test1", "password": "password1"}
+    response = client.post("/register", json=payload)
+    
+    assert response.status_code == 409
+    assert "already exists" in response.json()["detail"].lower()
+
+def test_register_invalid_data():
+    payload = {"username": "test2"}
+    response = client.post("/register", json=payload)
+
+    assert response.status_code == 422
+
+def test_login_success():
+    payload = {"username": "test1", "password": "password1"}
+    response = client.post("/login", json=payload)
+    
+    assert response.status_code == 200
+    assert "access_token" in response.json()
+
+def test_login_wrong_password():
+    payload = {"username": "test1", "password": "wrong1"}
+    response = client.post("/login", json=payload)
+    
+    assert response.status_code == 401
+
+def test_login_non_existent_user():
+    payload = {"username": "test2", "password": "password2"}
+
+    response = client.post("/login", json=payload)
+    assert response.status_code == 404
